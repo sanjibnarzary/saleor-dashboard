6caa1dbca87e3d79065680b9403fc080557cde4e
Wojciech Miśta <wojciech.mista@saleor.io>
2021-11-30 12:50:42 +0100

    Sorting hook uses useGiftCardList hook

--- src/giftCards/GiftCardsList/providers/GiftCardListProvider/GiftCardListProvider.tsx
+++ src/giftCards/GiftCardsList/providers/GiftCardListProvider/GiftCardListProvider.tsx
@@ -41,7 +41,7 @@ interface GiftCardsListProviderProps {
   params: GiftCardListUrlQueryParams;
 }
 
-export interface GiftCardListDataProps {
+export interface GiftCardListDataProps extends SortPage<GiftCardUrlSortField> {
   giftCards: Array<ExtendedGiftCard<GiftCardList_giftCards_edges_node>>;
   pageInfo: PageInfo;
   loading: boolean;
@@ -92,11 +92,14 @@ export const GiftCardsListProvider: React.FC<GiftCardsListProviderProps> = ({
   );
 
   const handleGiftCardListError = (error: ApolloError) => {
-    const { message } = error.graphQLErrors[0];
-    notify({
-      status: "error",
-      text: message
-    });
+    const { message } = error?.graphQLErrors[0];
+
+    if (message) {
+      notify({
+        status: "error",
+        text: message
+      });
+    }
   };
 
   const { data, loading } = useGiftCardListQuery({
